9c1b609597c17a66d7547b709ee3d63c2b2d4d9e
khuranamanan <112554344+khuranamanan@users.noreply.github.com>
2023-10-31 17:27:09 +0530

    refactor: Added message for Library when logged out/no song uploaded

--- components/Library.tsx
+++ components/Library.tsx
@@ -9,6 +9,7 @@ import useUploadModal from "@/hooks/useUploadMoadal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
 import useOnPlay from "@/hooks/useOnPlay";
+import Button from "./Button";
 
 interface LibraryProps {
   songs: Song[];
@@ -42,13 +43,40 @@ export default function Library({ songs }: LibraryProps) {
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-2">
-        {songs.map((song) => (
-          <MediaItem
-            key={song.id}
-            onClick={(id: string) => onPlay(id)}
-            data={song}
-          />
-        ))}
+        {songs.length === 0 ? (
+          <div className="flex flex-col items-center gap-2 w-full text-center text-neutral-400 text-sm mt-4">
+            {user && (
+              <>
+                <p>Add Songs to see it in your Library!</p>
+                <Button
+                  onClick={() => uploadModal.onOpen()}
+                  className="w-fit py-2"
+                >
+                  Add Songs
+                </Button>
+              </>
+            )}
+            {!user && (
+              <>
+                <p>Log in to see your library!</p>
+                <Button
+                  onClick={() => authModal.onOpen()}
+                  className="w-fit py-2"
+                >
+                  Log In
+                </Button>
+              </>
+            )}
+          </div>
+        ) : (
+          songs.map((song) => (
+            <MediaItem
+              key={song.id}
+              onClick={(id: string) => onPlay(id)}
+              data={song}
+            />
+          ))
+        )}
       </div>
     </div>
   );
